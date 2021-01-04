5c4707187cbc9c498f7477f084b9bbf0e2631f2a
fwfly <fawafwfly@gmail.com>
2021-01-04 20:25:34 +1100

    Print request header

--- proxy.c
+++ proxy.c
@@ -5,6 +5,8 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+void doit(int fd);
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
@@ -31,9 +33,29 @@ int main(int argc, char **argv)
         Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        //doit(connfd);                                             //line:netp:tiny:doit
+        doit(connfd);                                             //line:netp:tiny:doit
         Close(connfd);                                            //line:netp:tiny:close
     }
 
     return 0;
+}
+void doit(int fd)
+{
+    rio_t rio;
+    char buf[MAXLINE];
+    Rio_readinitb(&rio, fd);
+    if(!Rio_readlineb(&rio, buf, MAXLINE))
+        return;
+    printf("%s", buf);
+    // To do : Parsing method
+
+    while(strcmp(buf, "\r\n")){
+        if(!Rio_readlineb(&rio, buf, MAXLINE))
+            return;
+        printf("%s", buf);
+
+        // Open cliet fd for tiny
+        // send to tiny
+        // accept reply and print
+    }
 }
